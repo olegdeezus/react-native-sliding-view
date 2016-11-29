05e92162e83a6e58f71a48a8f2150db29563db00
Oleg Dizus <o.dizus@gmail.com>
2016-11-29 17:39:54 +0300

    Fix sorting bug

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 // @flow
 import React, { Component, PropTypes } from 'react'
 import {
-  View,
   Dimensions,
   Animated
 } from 'react-native'
-import * as Animatable from 'react-native-animatable'
 
 const deviceWidth = Dimensions.get('window').width
 const deviceHeight = Dimensions.get('window').height
 const BASE_CONTAINER_HEIGHT = 48
 
-export default class extends React.Component {
+const orderAsc = (a, b) => a - b
+
+export default class extends Component {
+  props: {
+    heights: number[],
+    onChange: (height: number) => void,
+    threshold: number,
+    tension: number,
+    friction: number,
+    children: Component<*, *, *>
+  }
   prevHeight: number
   heights: number[]
   oldY: number
@@ -35,7 +43,7 @@ export default class extends React.Component {
   }
 
   state = {
-    containerHeight: new Animated.Value(this.props.heights[0]),
+    containerHeight: new Animated.Value(this.props.heights[0])
   }
 
   constructor (props) {
@@ -44,7 +52,7 @@ export default class extends React.Component {
     const { heights } = props
 
     this.oldY = 0
-    this.heights = heights.sort()
+    this.heights = heights.sort(orderAsc)
     this.prevHeight = heights[0]
 
     this.state.containerHeight.setValue(this.prevHeight)
@@ -52,21 +60,21 @@ export default class extends React.Component {
 
   componentWillReceiveProps ({ heights }) {
     if (this.props.heights.toString() !== heights.toString()) {
-      heights.sort()
+      this.heights = heights.sort(orderAsc)
 
-      animate(heights[0])
+      this.animate(this.heights[0])
     }
   }
 
   handleTouchStart = ({ nativeEvent }) => {
-      this.oldY = nativeEvent.pageY
+    this.oldY = nativeEvent.pageY
   }
 
   handleTouchMove = ({ nativeEvent }) => {
     let newHeight = this.prevHeight + (this.oldY - nativeEvent.pageY)
 
-    const minHeight = this.heights.slice(-1)[0]
-    const maxHeight = this.heights[0]
+    const minHeight = this.heights[0]
+    const maxHeight = this.heights.slice(-1)[0]
 
     if (newHeight > minHeight && newHeight < maxHeight) {
       this.state.containerHeight.setValue(newHeight)
@@ -84,33 +92,33 @@ export default class extends React.Component {
   }
 
   handleTouchEnd = () => {
-      const { heights, prevHeight, animate } = this
-      const currentHeight = this.state.containerHeight._value
-      const prevIndex = heights.indexOf(prevHeight)
+    const { heights, prevHeight, animate } = this
+    const currentHeight = this.state.containerHeight._value
+    const prevIndex = heights.indexOf(prevHeight)
 
-      if (currentHeight - prevHeight >= this.props.threshold) {
-        return animate(heights[prevIndex - 1])
-      }
-      if (prevHeight - currentHeight >= this.props.threshold) {
-        return animate(heights[prevIndex + 1])
-      }
+    if (currentHeight - prevHeight >= this.props.threshold) {
+      return animate(heights[prevIndex + 1])
+    }
+    if (prevHeight - currentHeight >= this.props.threshold) {
+      return animate(heights[prevIndex - 1])
+    }
 
-      return animate(prevHeight)
+    return animate(prevHeight)
   }
 
   render () {
-      return <Animated.View
-          style={{
-              position: 'absolute',
-              bottom: 0,
-              height: this.state.containerHeight,
-              width: deviceWidth,
-          }}
-          onTouchStart={this.handleTouchStart}
-          onTouchMove={this.handleTouchMove}
-          onTouchEnd={this.handleTouchEnd}
-      >
-          {this.props.children}
-      </Animated.View>
+    return <Animated.View
+      style={{
+        position: 'absolute',
+        bottom: 0,
+        height: this.state.containerHeight,
+        width: deviceWidth
+      }}
+      onTouchStart={this.handleTouchStart}
+      onTouchMove={this.handleTouchMove}
+      onTouchEnd={this.handleTouchEnd}
+    >
+      {this.props.children}
+    </Animated.View>
   }
 }
